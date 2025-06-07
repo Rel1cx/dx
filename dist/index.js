979b591114d637d09018e1dea01f196e635b5cb7
Rel1cx <rel1cx@proton.me>
2025-06-07 22:11:20 +0800

    release: 0.0.22

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ import { AST_NODE_TYPES } from '@typescript-eslint/types';
 
 // package.json
 var name = "eslint-plugin-function";
-var version = "0.0.21";
+var version = "0.0.22";
 function getDocsUrl() {
   return "TODO: add docs for local ESLint rules";
 }
