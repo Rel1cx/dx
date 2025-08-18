ae2e1346a92b8f4a6493e1d8b08fa1f014287fd4
Rel1cx <dokimondex@gmail.com>
2025-08-19 03:25:24 +0800

    release: 0.0.24

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ import { unionConstituents } from "ts-api-utils";
 
 //#region package.json
 var name = "eslint-plugin-function";
-var version = "0.0.23";
+var version = "0.0.24";
 
 //#endregion
 //#region src/utils/create-rule.ts
