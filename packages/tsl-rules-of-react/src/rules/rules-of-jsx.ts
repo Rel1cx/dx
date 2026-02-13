10ea6b348413a7697dfea46fa6b55e328f10e8e4
Rel1cx <solarflarex@qq.com>
2026-02-13 21:42:53 +0800

    feat(tsl-rules-of-react): add key prop ordering rule for new JSX transform

--- packages/tsl-rules-of-react/src/rules/rules-of-jsx.ts
+++ packages/tsl-rules-of-react/src/rules/rules-of-jsx.ts
@@ -1,7 +1,15 @@
 import { type AST, defineRule } from "tsl";
 import ts from "typescript";
 
-export const messages = {};
+export const messages = {
+  /**
+   * Enforces 'key' prop placement before spread props.
+   */
+  keyMustBeforeSpread: "The 'key' prop must be placed before any spread props when using the new JSX transform.",
+  /**
+   * @todo: Add the rest of messages
+   */
+};
 
 /**
  * @todo: implement this
@@ -9,6 +17,45 @@ export const messages = {};
 export const rulesOfJsx = defineRule(() => {
   return {
     name: "rules-of-react/rules-of-jsx",
-    visitor: {},
+    createData(ctx) {
+      const {
+        jsx = "react-jsx",
+        // jsxFactory = "React.createElement",
+        // jsxFragmentFactory = "React.Fragment",
+        // jsxImportSource = "react",
+      } = ctx.program.getCompilerOptions();
+      return {
+        isNewJsxTransform: jsx === ts.JsxEmit.ReactJSX || jsx === ts.JsxEmit.ReactJSXDev,
+      };
+    },
+    visitor: {
+      JsxSpreadAttribute(ctx, node) {
+        if (!ctx.data.isNewJsxTransform) return;
+        const element = node.parent.parent;
+        if (element.kind !== ts.SyntaxKind.JsxOpeningElement && element.kind !== ts.SyntaxKind.JsxSelfClosingElement) {
+          return;
+        }
+        const children: ts.Node[] = [];
+        ts.forEachChild(element.attributes, (n) => void children.push(n));
+        let firstSpreadAttrIndex: null | number = null;
+        for (const [index, attr] of children.entries()) {
+          if (attr.kind === ts.SyntaxKind.JsxSpreadAttribute) {
+            firstSpreadAttrIndex ??= index;
+            continue;
+          }
+          if (firstSpreadAttrIndex == null) {
+            continue;
+          }
+          const name = attr.getChildAt(0);
+          // If a 'key' attribute is found after a spread attribute, report the keyMustBeforeSpread error
+          if (name.getText() === "key" && index > firstSpreadAttrIndex) {
+            ctx.report({
+              message: messages.keyMustBeforeSpread,
+              node: attr,
+            });
+          }
+        }
+      },
+    },
   };
 });
